5c7abc5b17a9548e95bc31c2cbe08154cb6ea2dd
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-03-18 09:49:57 -0300

    Configuring the webpack prod config at marketing

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,4 +1,12 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', 'public', 'index.html')
+    }),
+  ],
   module: {
     rules: [
       {
