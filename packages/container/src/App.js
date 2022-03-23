59122ad9426438349cd365071b5a44124f0461fe
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-03-23 14:35:31 -0300

    Updating text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from '../component/MarketingApp';
 function App() {
   return (
     <>
-      <h1>Container!!!</h1>
+      <h1>Container !!!</h1>
       <hr />
       <MarketingApp />
     </>
